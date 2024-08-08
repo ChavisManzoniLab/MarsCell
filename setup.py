4c67650223c7c74ba6a8e30384e146ea22c4a152
Julien Ferragu <julien.ferragu@inserm.fr>
2024-08-08 14:01:18 +0200

    Update

--- setup.py
+++ setup.py
@@ -3,7 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='MarCell',
     packages=find_packages(),
+    install_requires=[
+        'pandas==1.5.3',
+        'numpy==1.23.5',
+        'seaborn==0.12.2',
+        'matplotlib==3.7.2',
+        'plotly==5.18.0',
+        'scipy==1.10.1',
+        'cellpose==2.1.1']
+
     url='https://github.com/julienf1249/CACAO',
     author='Julien Ferragu',
     author_email='julien.ferragu@inserm.fr',
+     
 )
