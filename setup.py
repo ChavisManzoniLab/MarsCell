bf7a0d8f327e8d4717280ab73ac26e87efe42d03
julienf1249 <164188566+julienf1249@users.noreply.github.com>
2024-08-08 13:59:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='MarCell',
     packages=find_packages(),
-    url='https://github.com/julienf1249/CACAO',
+    url='https://github.com/julienf1249/MarCell',
     author='Julien Ferragu',
     author_email='julien.ferragu@inserm.fr',
 )
