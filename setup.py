b67801db61599ec713a8d86bbc1de83a7f3f8fce
Julien Ferragu <julien.ferragu@inserm.fr>
2024-08-08 13:28:03 +0200

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='MarCell',
-    packages=find_packages()
+    packages=find_packages(),
     url='https://github.com/julienf1249/CACAO',
     author='Julien Ferragu',
-    author_email='julien.ferragu@inserm.fr'
-    )
+    author_email='julien.ferragu@inserm.fr',
+)
