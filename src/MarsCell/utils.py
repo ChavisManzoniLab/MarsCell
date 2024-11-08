149c9571dc27ce0ec623c9d141d1066f42324387
Julien Ferragu <jousts.07-boxier@icloud.com>
2024-11-08 14:08:21 +0100

    ret

--- src/MarsCell/utils.py
+++ src/MarsCell/utils.py
@@ -5,13 +5,6 @@ from os.path import expanduser
 import fsspec
 import pathlib
 
-
-def download_files_from_github(folder_path):
-    destination = pathlib.Path(__file__).parent.resolve() / "test_folder_copy"
-    destination.mkdir(exist_ok=True, parents=True)
-    fs = fsspec.filesystem("github", org="ChavisManzoniLab", repo="MarCell")
-    fs.get(fs.ls("TAPAS_scripts/"), destination.as_posix())
-
 def initialisation():
     print('Startup')
 
