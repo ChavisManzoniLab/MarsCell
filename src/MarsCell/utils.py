21517ce3e3ad7d9f4b990578726f70fe0da16c7d
Julien Ferragu <jousts.07-boxier@icloud.com>
2024-11-08 13:46:25 +0100

    updtate

--- src/MarsCell/utils.py
+++ src/MarsCell/utils.py
@@ -15,34 +15,4 @@ def download_files_from_github(folder_path):
 def initialisation():
     print('initialisation')
 
-    def save_project_data(data, filename= os.path.join(home, "..","MarCell_data.yaml")):
-        with open(filename, 'w') as file:
-            yaml.dump(data, file, default_flow_style=False)
-            
-
-    def load_project_data(filename=os.path.join(home,"MarCell_data.yaml")):
-        with open(filename, 'r') as file:
-            data = yaml.safe_load(file)
-        return data
-
-    # Example project data
-    project_info = {
-        "name": "MarCell_data",
-        "description": "This is a storage file for data useful to MarCell.",
-        "convention": {
-        },
-        "tapas_path": 
-        "cellpose_model": "runCellpose2D-reelin-2023bat.bat",
-    }
-
-    # Save the data to YAML
-    """if not os.path.isdir(os.path.join(script_directory, "..","MarCell_data.yaml")):
-        print(not os.path.isdir(os.path.join(script_directory, "..","MarCell_data.yaml")))
-        save_project_data(project_info)
-        print('saved')
-    else:
-        print('already a doc')"""
-
-    # Load the data from YAML
-
     
