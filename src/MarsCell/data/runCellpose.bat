757dd4b7d6995639bce919bfb5a04cb6f40951d2
julienf1249 <164188566+julienf1249@users.noreply.github.com>
2024-11-14 17:33:53 +0100

    file to use to run Cellpose

---
+++ src/MarsCell/data/runCellpose.bat
@@ -0,0 +1,1 @@
+path_cellpose_env -m cellpose --dir %1 --chan 0 --pretrained_model path_model_Reelin_P40_advanced --diameter .0 --save_tif --no_npy --stitch_threshold 0.4 --use_gpu --anisotropy 4 --cellprob_threshold -2.0 --flow_threshold 0.5
